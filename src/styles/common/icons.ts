9cd4d17920a401f2a765083cd1c389342f546dbe
ccconac <qwerpoiu91@naver.com>
2024-07-23 04:00:43 +0900

    move: 메뉴 바 icon 추가 및 경로 상수화

--- src/styles/common/icons.ts
+++ src/styles/common/icons.ts
@@ -3,4 +3,13 @@ export const ICONS = {
   logoImage: '/assets/logo/cushion-vector-asset.svg',
   backButton: '/assets/icon/button-arrow-left-line.svg',
   moreButton: '/assets/icon/button-more-line.svg',
+
+  menu: {
+    delete: '/assets/icon/menu/button-delete-bin-2-line.svg',
+    externalLink: '/assets/icon/menu/button-external-link-line.svg',
+    edit: '/assets/icon/menu/button-file-edit-line.svg',
+    quit: '/assets/icon/menu/button-logout-box-line.svg',
+    logout: '/assets/icon/menu/button-logout-circle-line.svg',
+    divider: '/assets/icon/menu/divider.svg',
+  },
 };

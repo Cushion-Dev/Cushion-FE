c66608d682369cfec2cec8ee0358bccf72430b9c
ccconac <qwerpoiu91@naver.com>
2024-07-22 18:07:02 +0900

    feat: 디자인 시스템 semantic color 상수화

---
+++ src/styles/semantic.ts
@@ -0,0 +1,211 @@
+export const semantic = {
+  light: {
+    accent: {
+      solid: {
+        hero: '#D97706',
+        normal: '#E78A09',
+      },
+      transparent: {
+        hero: 'rgba(217, 119, 6, 0.16)',
+        normal: 'rgba(231, 138, 9, 0.16)',
+      },
+    },
+
+    object: {
+      solid: {
+        hero: '#0C0A09',
+        normal: '#231F1E',
+      },
+      transparent: {
+        neutral: 'rgba(55, 51, 48, 0.75)',
+        alternative: 'rgba(55, 51, 48, 0.63)',
+        assistive: 'rgba(55, 51, 48, 0.43)',
+        disabled: 'rgba(55, 51, 48, 0.22)',
+      },
+    },
+
+    bg: {
+      solid: {
+        lighter: '#FFF',
+        light: '#FFF',
+        normal: '#FAFAF7',
+        heavy: '#F5F5F3',
+        heavier: '#EEEDEC',
+        lightReversed: '#F5F5F3',
+        heavyReversed: '#EEEDEC',
+      },
+      transparent: {
+        dimmed: 'rgba(12, 10, 9, 0.43)',
+        overlay: 'rgba(250, 250, 249, 0.63)',
+        normalClear: 'rgba(250, 250, 247, 0.00)',
+      },
+    },
+
+    border: {
+      solid: {
+        neutral: '#E7E5E4',
+        alternative: '#EEEDEC',
+        assistive: '#F5F5F3',
+      },
+      transparent: {
+        neutral: 'rgba(87, 83, 78, 0.22)',
+        alternative: 'rgba(87, 83, 78, 0.16)',
+        assistive: 'rgba(87, 83, 78, 0.08)',
+      },
+    },
+
+    fill: {
+      transparent: {
+        normal: 'rgba(87, 83, 78, 0.22)',
+        neutral: 'rgba(87, 83, 78, 0.12)',
+        alternative: 'rgba(87, 83, 78, 0.08)',
+        assistive: 'rgba(87, 83, 78, 0.05)',
+      },
+    },
+
+    feedback: {
+      solid: {
+        positive: '#16A34A',
+        caution: '#CA8A04',
+        negative: '#DC2626',
+      },
+      transparent: {
+        positive: 'rgba(22, 163, 74, 0.16)',
+        caution: 'rgba(202, 138, 4, 0.16)',
+        negative: 'rgba(220, 38, 38, 0.16)',
+      },
+    },
+
+    interactive: {
+      solid: {
+        normal: '#57534E',
+        inversed: '#D6D3D1',
+        disabled: '#F5F5F3',
+        focused: '#93C5FD',
+      },
+    },
+
+    base: {
+      solid: {
+        white: '#FFF',
+        black: '#000',
+      },
+    },
+
+    inversed: {
+      solid: {
+        accent: '#E78A09',
+        negative: '#EF4444',
+        primary: '#FFF',
+        secondary: '#EEEDEC',
+        disabled: '#44403C',
+        background: '#292524',
+      },
+    },
+  },
+
+  dark: {
+    accent: {
+      solid: {
+        hero: '#E78A09',
+        normal: '#F59E0B',
+      },
+      transparent: {
+        hero: 'rgba(231, 138, 9, 0.22)',
+        normal: 'rgba(245, 158, 11, 0.22)',
+      },
+    },
+
+    object: {
+      solid: {
+        hero: '#FFF',
+        normal: '#FAFAF7',
+      },
+      transparent: {
+        neutral: 'rgba(214, 211, 209, 0.75)',
+        alternative: 'rgba(214, 211, 209, 0.63)',
+        assistive: 'rgba(214, 211, 209, 0.43)',
+        disabled: 'rgba(214, 211, 209, 0.22)',
+      },
+    },
+
+    bg: {
+      solid: {
+        lighter: '#292524',
+        light: '#231F1E',
+        normal: '#201C1B',
+        heavy: '#1C1917',
+        heavier: '#141210',
+        lightReversed: '#292524',
+        heavyReversed: '#373330',
+      },
+      transparent: {
+        dimmed: 'rgba(12, 10, 9, 0.63)',
+        overlay: 'rgba(28, 25, 23, 0.63)',
+        normalClear: 'rgba(32, 28, 27, 0.00)',
+      },
+    },
+
+    border: {
+      solid: {
+        neutral: '#373330',
+        alternative: '#292524',
+        assistive: '#231F1E',
+      },
+      transparent: {
+        neutral: 'rgba(87, 83, 78, 0.35)',
+        alternative: 'rgba(87, 83, 78, 0.28)',
+        assistive: 'rgba(87, 83, 78, 0.16)',
+      },
+    },
+
+    fill: {
+      transparent: {
+        normal: 'rgba(87, 83, 78, 0.28)',
+        neutral: 'rgba(87, 83, 78, 0.22)',
+        alternative: 'rgba(87, 83, 78, 0.16)',
+        assistive: 'rgba(87, 83, 78, 0.08)',
+      },
+    },
+
+    feedback: {
+      solid: {
+        positive: '#22C55E',
+        caution: '#EAB308',
+        negative: '#EF4444',
+      },
+      transparent: {
+        positive: 'rgba(34, 197, 94, 0.22)',
+        caution: 'rgba(234, 179, 8, 0.22)',
+        negative: 'rgba(239, 68, 68, 0.22)',
+      },
+    },
+
+    interactive: {
+      solid: {
+        normal: '#D6D3D1',
+        inversed: '#57534E',
+        disabled: '#373330',
+        focused: '#BFDBFE',
+      },
+    },
+
+    base: {
+      solid: {
+        white: '#FFF',
+        black: '#000',
+      },
+    },
+
+    inversed: {
+      solid: {
+        accent: '#F59E0B',
+        negative: '#DC2626',
+        primary: '#44403C',
+        secondary: '#44403C',
+        disabled: '#DFDCDB',
+        background: '#F5F5F3',
+      },
+    },
+  },
+};

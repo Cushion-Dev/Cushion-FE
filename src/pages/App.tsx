d09987d9a590dc00e88a2c38afa80c549ab9a333
ccconac <qwerpoiu91@naver.com>
2024-07-22 19:01:42 +0900

    chore: router 파일 및 App 경로 추가

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { TYPO } from '../styles/typo';
+
+const App = () => {
+  return <Font>확인</Font>;
+};
+
+export default App;
+
+const Font = styled.h2`
+  ${TYPO.display1}
+`;

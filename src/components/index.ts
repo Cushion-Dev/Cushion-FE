2b9c9237dc154e2d4d86f801979e32fe361a8bc5
ccconac <qwerpoiu91@naver.com>
2024-07-23 13:12:34 +0900

    feat: 팝오버 컴포넌트 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as ContextMenu } from './common/MenuBar/ContextMenu';
 
 export { default as Dialog } from './common/Dialog/Dialog';
 export { default as LoginDialog } from './common/Dialog/LoginDialog';
+
+export { default as Popover } from './common/Popover/Popover';
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,54 +1,18 @@
-import { Dialog, LoginDialog } from '../components';
-import { MESSAGES } from '../constants/messages';
+import { Popover } from '../components';
+import styled from 'styled-components';
 
 const App = () => {
   return (
-    <>
-      <Dialog
-        variant="cta"
-        titleText={MESSAGES.dialog.alert.title}
-        subText={MESSAGES.dialog.alert.sub}
-        cancelText={MESSAGES.dialog.alert.no}
-        eventText={MESSAGES.dialog.alert.yes}
-      />
-      <Dialog
-        variant="cta"
-        titleText={MESSAGES.dialog.ocr.title}
-        subText={MESSAGES.dialog.ocr.sub}
-        cancelText={MESSAGES.dialog.ocr.cancel}
-        eventText={MESSAGES.dialog.ocr.attach}
-      />
-      <Dialog
-        variant="cta"
-        titleText={MESSAGES.dialog.logout.title}
-        subText={MESSAGES.dialog.logout.sub}
-        cancelText={MESSAGES.dialog.logout.cancel}
-        eventText={MESSAGES.dialog.logout.logout}
-      />
-      <Dialog
-        variant="negative"
-        titleText={MESSAGES.dialog.withdraw.title}
-        subText={MESSAGES.dialog.withdraw.sub}
-        cancelText={MESSAGES.dialog.withdraw.cancel}
-        eventText={MESSAGES.dialog.withdraw.withdraw}
-      />
-      <Dialog
-        variant="negative"
-        titleText={MESSAGES.dialog.delete.title}
-        subText={MESSAGES.dialog.delete.sub}
-        cancelText={MESSAGES.dialog.delete.cancel}
-        eventText={MESSAGES.dialog.delete.delete}
-      />
-      <Dialog
-        variant="cta"
-        titleText={MESSAGES.dialog.back.title}
-        subText={MESSAGES.dialog.back.sub}
-        cancelText={MESSAGES.dialog.back.no}
-        eventText={MESSAGES.dialog.back.yes}
-      />
-      <LoginDialog />
-    </>
+    <Container>
+      <Popover title="팝오버 타이틀" bodyText="팝오버 문장은 최대 3줄까지 입력 가능합니다." />
+    </Container>
   );
 };
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;

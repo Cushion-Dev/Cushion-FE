9c48f31109aeba70e58a1076f0297a79fd267136
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-07-23 15:15:03 +0900

    FEAT: hover, active 이벤트 구현

--- src/components/common/ListItem/ListItem.tsx
+++ src/components/common/ListItem/ListItem.tsx
@@ -37,6 +37,15 @@ const ListItemContainer = styled.div`
   border: 1px solid ${semantic.light.border.transparent.neutral};
   background: ${semantic.light.bg.solid.light};
   box-shadow: 0px 0px 5.599999904632568px rgba(12, 10, 9, 0.06);
+  cursor: pointer;
+
+  &:hover {
+    background: #f3f3f3;
+  }
+
+  &:active {
+    background: #f3f3f3;
+  }
 `;
 
 const ListHeader = styled.div`

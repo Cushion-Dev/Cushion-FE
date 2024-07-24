2ead1269ca6ba165c6f49644bb29cf301d149203
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-07-24 14:16:57 +0900

    FEAT: UserSetting 페이지, ChatList 페이지 라우팅

--- src/router.tsx
+++ src/router.tsx
@@ -2,8 +2,9 @@ import { createBrowserRouter } from 'react-router-dom';
 import Onboarding from './pages/Onboarding';
 import CreateCushion from './pages/CreateCushion';
 import UserSetting from './pages/UserSetting';
-import Test from './pages/Test';
+
 import App from './pages/App';
+import ChatList from './pages/ChatList';
 
 const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
   {
@@ -12,11 +13,19 @@ const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
   },
   {
     path: '/',
-    element: <Test />,
+    element: <App />,
   },
   {
     path: '/cushion/:id',
     element: <CreateCushion />,
+  },
+  {
+    path: '/chat-list/:id',
+    element: <ChatList></ChatList>,
+  },
+  {
+    path: '/user-setting',
+    element: <UserSetting></UserSetting>,
   },
 ]);
 

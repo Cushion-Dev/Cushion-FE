7476a59135757e59ea2bb15d70248ae0c54f9423
ccconac <qwerpoiu91@naver.com>
2024-07-28 13:01:41 +0900

    feat: callout 이용 약관 이동 추가

--- src/components/common/Dialog/Callout.tsx
+++ src/components/common/Dialog/Callout.tsx
@@ -22,7 +22,11 @@ const Callout = ({ text, variant }: CalloutProps) => {
 
         {variant === 'login' ? (
           <CalloutText>
-            로그인 시, 쿠션의 <Terms href="/">이용약관</Terms>에 동의하는 것으로 간주돼요.
+            {`로그인 시, 쿠션의 `}
+            <Terms href="https://pentagonal-tuna-017.notion.site/26ccf78bb7234d4cbdbbbbf503363b99?pvs=4">
+              이용약관
+            </Terms>
+            에 동의하는 것으로 간주돼요.
           </CalloutText>
         ) : (
           <CalloutText>{text}</CalloutText>

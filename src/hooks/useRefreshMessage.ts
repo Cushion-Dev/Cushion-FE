3b22c5ed717dc2485968c9779b54a5076a3d086b
ccconac <qwerpoiu91@naver.com>
2024-08-07 18:21:24 +0900

    feat: 새로 고침 시 메시지 생성 로딩 표시

--- src/hooks/useRefreshMessage.ts
+++ src/hooks/useRefreshMessage.ts
@@ -1,26 +1,30 @@
-import { UseMutationResult, useMutation } from '@tanstack/react-query';
 import axios from 'axios';
+import { UseMutationResult, useMutation } from '@tanstack/react-query';
+import { useMessageLoading } from '../stores/Modal/useModalStore';
 
 interface RefreshMessageParams {
   roomId: number;
   withPersonality: boolean;
 }
 
-const postRefreshMessage = async ({
-  roomId,
-  withPersonality,
-}: RefreshMessageParams): Promise<any> => {
+const postRefreshMessage = async (params: RefreshMessageParams): Promise<any> => {
+  const { roomId, withPersonality } = params;
   const response = await axios.post('/change-style/retry', {
     roomId,
     withPersonality,
   });
+
   return response.data;
 };
 
 const useRefreshMessage = (): UseMutationResult<any, Error, RefreshMessageParams> => {
-  return useMutation(postRefreshMessage, {
-    onSuccess: (data: any) => {
-      return data.message;
+  const { close: closeMessageLoading } = useMessageLoading();
+
+  return useMutation({
+    mutationFn: postRefreshMessage,
+    onSuccess: (data) => {
+      closeMessageLoading();
+      console.log('새로 고침 메시지', data.message);
     },
     onError: (error) => {
       console.error('새로 고침을 할 수 없습니다.', error);
